f0deb51d4cf377cbe2d940232ec63e73f298ba18
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2006-03-29 16:03:16 +0000

    Working test for EnergyAlgos (limited functionality)

--- test/testEnergyAlgos.cpp
+++ test/testEnergyAlgos.cpp
@@ -1,5 +1,5 @@
 /*! \file testEnergyAlgos.cpp
- * \brief Procedural skeleton unit-test code for the L1GctJetFinder class.
+ * \brief Procedural skeleton unit-test code for the L1Gct(...)EnergyAlgos classes.
  *
  * Update comment when we have decided what the code does!
  *  This is skeleton code that reads in data from a text file to feed into
@@ -15,7 +15,7 @@
 //NOTE all these includes need to be sorted with a proper build include path,
 //rather than a relative path, in order to comply with CMS style.
 
-// #include "L1Trigger/GlobalCaloTrigger/interface/L1GctJetFinder.h"  //The class to be tested
+#include "L1Trigger/GlobalCaloTrigger/interface/L1GctGlobalEnergyAlgos.h"  //The class to be tested
 
 // //Custom headers needed for this test
 // #include "L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h"
@@ -37,6 +37,10 @@ using namespace std;
 //  FUNCTION PROTOTYPES
 /// Runs the test, and returns a string with the test result message in.
 string classTest();
+//
+/// Generates test data consisting of energies to be added together with their sum
+void generateTestData(vector<unsigned> &energies, int size, unsigned max, unsigned &sum);
+
 // /// Loads test input regions and also the known results from a text file.
 // void loadTestData(RegionsVector &regions, JetsVector &jets, const string &fileName);
 // /// Function to safely open files of any name, using a referenced return ifstream
@@ -49,6 +53,7 @@ int main(int argc, char **argv)
     cout << "Hello from testEnergyAlgos       " << endl;
     cout << "*********************************" << endl;
 
+    srand(290306);
     try
     {
         cout << "\n" << classTest() << endl;
@@ -67,66 +72,96 @@ int main(int argc, char **argv)
 // Runs the test, and returns a string with the test result message in.
 string classTest()
 {
-//     L1GctJetFinder myJetFinder; //TEST OBJECT;    
-//     bool testPass = true;       //Test passing flag.
+  L1GctGlobalEnergyAlgos myGlobalEnergy;
+  bool testPass = true;       //Test passing flag.
     
-//     // Number of calorimter regions to be fed to the jet finder.
-//     const int maxRegions = 64;  //64 based on old GCT design
-    
-//     // Name of the file containing the test input data.
-//     const string testDataFile = "JetFinderTesterData.txt";  
-    
-//     // Vectors for reading in test data from the text file.
-//     RegionsVector inputRegions(maxRegions);
-//     JetsVector correctJets;
-//     // Vectors for receiving the output from the object under test.
-//     RegionsVector outputRegions(maxRegions);
-//     JetsVector outputJets;
-    
-//     // Load our test input data and known results
-//     loadTestData(inputRegions, correctJets, testDataFile);
-    
-//     //Fill the L1GctJetFinder with regions.
-//     for(int i = 0; i < maxRegions; ++i)
-//     {
-//         myJetFinder.setInputRegion(i, inputRegions[i]);
-//     }
+  const int noOfValues=3;
+  vector<unsigned> energyValues(noOfValues);
+  unsigned energySum;
 
-//     // Test the getInputRegion method
-//     outputRegions = myJetFinder.getInputRegions();
-//     if(outputRegions.size() != inputRegions.size())  
-//     {
-//         testPass = false;
-//     }
-//     else
-//     {
-//         for(int i = 0; i < maxRegions; ++i)
-//         {
-//             if(outputRegions[i].getEt() != inputRegions[i].getEt()) { testPass = false; break; }
-//             if(outputRegions[i].getMip() != inputRegions[i].getMip()) { testPass = false; break; }
-//             if(outputRegions[i].getQuiet() != inputRegions[i].getQuiet()) {testPass = false; break; }
-//         }
-//     }
-        
-//     if(testPass == false)
-//     {
-//         return "Test class has failed initial data input/output comparison!";
-//     }
-    
-//     myJetFinder.process();  //Run algorithm
-    
-//     //Get and then test the output jets against known results
-//     //NEEDS FILLING IN SIMILARLY TO ABOVE
-//     outputJets = myJetFinder.getJets();
-//     // **Fill in***
-    
-    
-//     if(testPass == false)
-//     {
-//         return "Test class has failed algorithm processing!";
-//     }
+  unsigned EtHadResult;
 
+  const int nbitsEt=12;
+  const unsigned energyMax=((1<<nbitsEt) - 1);
+
+  const int maxTests=1000;
+
+  for (int t=0; t<maxTests; t++)
+    {
+      // Initialise the myGlobalEnergy object
+      myGlobalEnergy.reset();
+
+      // For each of the different types of energy sum,
+      // generate our test input data and known results.
+      // Load the input data into the algorithm and store
+      // a local copy of the output.
+      //
+      // Jet energy sums (Ht):
+      generateTestData(energyValues, noOfValues, energyMax, energySum);
+
+      cout << "\nEnergy " << energyValues[0];
+      cout << "\nEnergy " << energyValues[1];
+      cout << "\nEnergy " << energyValues[2];
+
+      // Fill the L1GctGlobalEnergyAlgos
+      myGlobalEnergy.setInputWheelHt(0, energyValues[0], false);
+      myGlobalEnergy.setInputWheelHt(1, energyValues[1], false);
+      myGlobalEnergy.setInputBoundaryHt(energyValues[2], false);
+
+      // Test the GetInput... methods
+      if (myGlobalEnergy.getInputHtValPlusWheel() != energyValues[0]) { testPass = false; }
+      if (myGlobalEnergy.getInputHtVlMinusWheel() != energyValues[1]) { testPass = false; }
+      if (myGlobalEnergy.getInputHtBoundaryJets() != energyValues[2]) { testPass = false; }
+            
+      // Local storage of the sum
+      if (energySum <= energyMax)
+	{ EtHadResult = energySum; }
+      else
+	{ EtHadResult = (energySum & energyMax) | (1<<nbitsEt); }
+
+      cout << "\nSum " << energySum << " result " << EtHadResult << endl;
+
+      if(testPass == false)
+	{
+	  return "Test class has failed initial data input/output comparison!";
+	}
+    
+      myGlobalEnergy.process();  //Run algorithm
+
+      cout << "\nAlgo output " << myGlobalEnergy.getEtHad() << endl;
+      // Check the output values
+      if (myGlobalEnergy.getEtHad() != EtHadResult) {testPass = false;}
+    
+    
+      if(testPass == false)
+	{
+	  return "Test class has failed algorithm processing!";
+	}
+    }
     return "Test class has passed!";        
+}
+
+/// Generates test data consisting of energies to be added together with their sum
+void generateTestData(vector<unsigned> &energies, int size, unsigned max, unsigned &sum)
+{
+  int i;
+  int r,e;
+  float p,q,s,t;
+  int tempsum;
+
+  tempsum = 0;
+  p = float(max);
+  q = float(RAND_MAX);
+  for (i=0; i<size; i++) {
+    r = rand();
+    s = float(r);
+    t = s*p/q;
+    e = int(t);
+
+    energies[i] = e;
+    tempsum = tempsum+e;
+  }
+  sum = tempsum;
 }
 
 
