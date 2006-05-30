62e5d9c7fec20904d6b22798d231aae62683c72e
mfhansen <sha1-4c6d66dbcfddc4349cf1514bfba4442abf4433f7@cern.ch>
2006-05-30 14:01:24 +0000

    updated

--- test/testElectrons.cpp
+++ test/testElectrons.cpp
@@ -19,6 +19,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>  //for int->char conversion
 
 using std::cout;
 using std::endl;
@@ -29,6 +30,8 @@ int main()
 {
  
   L1GlobalCaloTrigger* gct = new L1GlobalCaloTrigger();
+  
+  //gct->print();
   //Firstly, check that there's nothing in the buffers of the GCT
   vector<L1GctEmCand> electrons = gct->getIsoElectrons();
   vector<L1GctEmCand> nonIso = gct->getNonIsoElectrons();
@@ -39,29 +42,18 @@ int main()
     "         "<<nonIso[i].rank()<<"      "<<nonIso[i].eta()<<"     "<<nonIso[i].phi()<<endl;
   }
 
-  //Open a source card file and look at the electrons in there
-  vector<L1GctSourceCard*> theSourceCards = gct->getSourceCards();
-  for(unsigned int i=0;i!=(theSourceCards.size()/3);i++){
-    //These ready for later when gotten more than one BX/file to read in
-    //  theSourceCards[i]->openInputFile("testSourceCardInput.txt");
-    //  theSourceCards[i*3]->readBX();
-    //First checking buffers before reading in data
-  vector<L1GctEmCand> isoElectrons = theSourceCards[i*3]->getIsoElectrons();
-  vector<L1GctEmCand> nonIsoElectrons = theSourceCards[i*3]->getNonIsoElectrons();
-  cout<<"For source card no: "<<i<<"  Rank   Eta   Phi"<<endl;
-  for(unsigned int n=0;n!=isoElectrons.size();n++){
-    cout<<"Iso electrons are:      "<<isoElectrons[n].rank()<<"      "<<isoElectrons[n].eta()<<"     "<<isoElectrons[n].phi()<<endl;
-    cout<<"Non-iso are:            "<<nonIsoElectrons[n].rank()<<"      "<<isoElectrons[n].eta()<<"     "<<isoElectrons[n].phi()<<endl;
-  }
-  }
-  theSourceCards[0]->openInputFile("testSourceCardInput.txt");
-  theSourceCards[0]->readBX();
-  vector<L1GctEmCand> iso_zero = theSourceCards[0]->getIsoElectrons();
-  vector<L1GctEmCand> nonIso_zero = theSourceCards[0]->getNonIsoElectrons();
-  cout<<"For source card no  0   Rank   Eta   Phi"<<endl;
-   for(unsigned int n=0;n!=iso_zero.size();n++){
-     cout<<"Iso electrons are:      "<<std::hex<<iso_zero[n].rank()<<"      "<<std::hex<<iso_zero[n].eta()<<"     "<<std::hex<<iso_zero[n].phi()<<endl;
-      cout<<"Non-iso are:            "<<nonIso_zero[n].rank()<<"      "<<nonIso_zero[n].eta()<<"     "<<nonIso_zero[n].phi()<<endl;
-   }
+  //Open source card files and look at the electrons in there
+    std::string fileName = "testElectronsRct_";
+    gct->openSourceCardFiles(fileName);
+    gct->process();
+    gct->print();
+    vector<L1GctEmCand> newIso = gct->getIsoElectrons();
+    vector<L1GctEmCand> newnonIso = gct->getNonIsoElectrons();
+    cout<<"From GCT: iso electrons     &    non-iso electrons:"<<endl;
+    cout<<"          Rank   Eta   Phi       Rank   Eta   Phi" <<endl;
+    for(unsigned int i=0;i!=electrons.size();i++){
+      cout<<"          "<<newIso[i].rank()<<"      "<<newIso[i].eta()<<"     "<<newIso[i].phi()<<
+	"         "<<newnonIso[i].rank()<<"      "<<newnonIso[i].eta()<<"     "<<newnonIso[i].phi()<<endl;
+    }
 }
 
