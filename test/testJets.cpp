92b027cecbcc885aa08574ef1a51e1a1ee707ede
Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch>
2006-06-02 14:35:34 +0000

    Opens a set of input source cards, processes them and output the final jets.

--- test/testJets.cpp
+++ test/testJets.cpp
@@ -1,5 +1,5 @@
 /*! \file testJets.cpp
- * \test file for testing the full GCT 
+ * \test file for testing the full GCT class jet algorithms
  *
  *  
  *
@@ -7,54 +7,66 @@
  * \date May 2006
  */
 
-
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GlobalCaloTrigger.h" 
-#include "L1Trigger/GlobalCaloTrigger/interface/L1GctSourceCard.h" 
-#include "L1Trigger/GlobalCaloTrigger/interface/L1GctEmCand.h"
+#include "FWCore/Utilities/interface/Exception.h"
 
-//Standard library headers
-#include <fstream>   //for file IO
-#include <string>
-#include <vector>
 #include <iostream>
+#include <exception>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-  L1GlobalCaloTrigger* gct = new L1GlobalCaloTrigger();
-  gct->print();
-  //Firstly, check that there's nothing in the buffers of the GCT
-//   std::vector<L1GctEmCand> electrons = gct->getIsoElectrons();
-//   std::vector<L1GctEmCand> nonIso = gct->getNonIsoElectrons();
-//   std::cout<<"From GCT: iso electrons     &    non-iso electrons:"<<std::endl;
-//   std::cout<<"          Rank   Eta   Phi       Rank   Eta   Phi" <<std::endl;
-//   for(unsigned int i=0;i!=electrons.size();i++){
-//     std::cout<<"          "<<electrons[i].rank()<<"      "<<electrons[i].eta()<<"     "<<electrons[i].phi()<<
-//     "         "<<nonIso[i].rank()<<"      "<<nonIso[i].eta()<<"     "<<nonIso[i].phi()<<std::endl;
-//   }
+  try {
+    // New GCT
+    L1GlobalCaloTrigger* gct = new L1GlobalCaloTrigger(true); 
+    cout << "Reset the GCT" << endl;
 
-//   //Open a source card file and look at the electrons in there
-//   vector<L1GctSourceCard*> theSourceCards = gct->getSourceCards();
-//   for(unsigned int i=0;i!=(theSourceCards.size()/3);i++){
-//     //These ready for later when gotten more than one BX/file to read in
-//     //  theSourceCards[i]->openInputFile("testSourceCardInput.txt");
-//     //  theSourceCards[i*3]->readBX();
-//     //First checking buffers before reading in data
-//   vector<L1GctEmCand> isoElectrons = theSourceCards[i*3]->getIsoElectrons();
-//   vector<L1GctEmCand> nonIsoElectrons = theSourceCards[i*3]->getNonIsoElectrons();
-//   cout<<"For source card no: "<<i<<"  Rank   Eta   Phi"<<endl;
-//   for(unsigned int n=0;n!=isoElectrons.size();n++){
-//     cout<<"Iso electrons are:      "<<isoElectrons[n].rank()<<"      "<<isoElectrons[n].eta()<<"     "<<isoElectrons[n].phi()<<endl;
-//     cout<<"Non-iso are:            "<<nonIsoElectrons[n].rank()<<"      "<<isoElectrons[n].eta()<<"     "<<isoElectrons[n].phi()<<endl;
-//   }
-//   }
-//   theSourceCards[0]->openInputFile("testSourceCardInput.txt");
-//   theSourceCards[0]->readBX();
-//   vector<L1GctEmCand> iso_zero = theSourceCards[0]->getIsoElectrons();
-//   vector<L1GctEmCand> nonIso_zero = theSourceCards[0]->getNonIsoElectrons();
-//   cout<<"For source card no  0   Rank   Eta   Phi"<<endl;
-//    for(unsigned int n=0;n!=iso_zero.size();n++){
-//      cout<<"Iso electrons are:      "<<std::hex<<iso_zero[n].rank()<<"      "<<std::hex<<iso_zero[n].eta()<<"     "<<std::hex<<iso_zero[n].phi()<<endl;
-//       cout<<"Non-iso are:            "<<nonIso_zero[n].rank()<<"      "<<nonIso_zero[n].eta()<<"     "<<nonIso_zero[n].phi()<<endl;
-//    }
+    // clear everything
+    gct->reset(); 
+
+    // Open source card input files
+    cout << "Opening input source card files" << endl;
+    gct->openSourceCardFiles("RCT_"); 
+
+    // Run
+    cout << "Run the GCT...." << endl;
+    gct->process(); // process
+
+    // central jet outputs to GT
+    vector<L1GctJetCand> centralJets = gct->getCentralJets();
+  
+    // forward jet outputs to GT
+    vector<L1GctJetCand> forwardJets = gct->getForwardJets();
+  
+    // tau jet outputs to GT
+    vector<L1GctJetCand> tauJets = gct->getTauJets();
+
+    // Now take a look at the output jets
+
+    cout << "===== CENTRAL JETS ====" << endl;
+    for (unsigned i=0; i<centralJets.size();i++){
+      cout << centralJets[i];
+    }
+
+    cout << "===== FORWARD JETS ====" << endl;
+    for (unsigned i=0; i<forwardJets.size();i++){
+      cout << forwardJets[i];
+    }
+
+    cout << "===== TAU JETS ====" << endl;
+    for (unsigned i=0; i<tauJets.size();i++){
+      cout << tauJets[i];
+    }
+    
+  }
+  catch (cms::Exception& e)
+    {
+      cerr << e.what() << endl;
+    }
+  catch (...) { // Catch anything unknown that goes wrong! 
+    cerr << "yikes! something awful happened.... (unknown exception)" << endl; 
+  } 
 }
 
